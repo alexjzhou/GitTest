25ffb73b43802582687eef5787cabfec1198e23e
Alexandra Jinyu Zhou <AlexandraJZhou@AlexandrasMBP3.frontierlocal.net>
2018-01-14 20:35:36 -0800

    added a line in readme

---
+++ index.cpp
@@ -0,0 +1,8 @@
+//
+//  index.cpp
+//  
+//
+//  Created by Alexandra Jinyu Zhou on 2018-01-14.
+//
+
+#include "index.hpp"
---
+++ index.hpp
@@ -0,0 +1,13 @@
+//
+//  index.hpp
+//  
+//
+//  Created by Alexandra Jinyu Zhou on 2018-01-14.
+//
+
+#ifndef index_hpp
+#define index_hpp
+
+#include <stdio.h>
+
+#endif /* index_hpp */
